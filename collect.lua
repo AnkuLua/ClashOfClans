192f62eaffbc74f8ac4d1e8ab6160dcb849b4e77
AnkuLua <ankulua@gmail.com>
2015-12-23 22:28:36 +0800

    move common functions to COCLib.lua check "supercellLogo.png" to disconnected click(alive) to keep connection alive and remove extra dialogs show collect and reload counts

--- collect.lua
+++ collect.lua
@@ -1,38 +1,9 @@
-function existsClickAll(target, seconds)
-    if (not exists(target, seconds)) then return end
-    usePreviousSnap(true)
-    local all = findAll(target, 0)
-    usePreviousSnap(false)
---    for i, r in ipairs(all) do
---        r:highlight()
---    end
-
---    wait(2)
---    for i, r in ipairs(all) do
---        r:highlight()
---    end
---
-    for i, r in ipairs(all) do
-        click(r)
-    end
-end
-
-function eraseStone() -- stone is similar to dark elixir. erase them to avoid confusion
-    setHighlightStyle(0xffffff00, true)
-    if (not exists(Pattern("stone.png"):similar(0.9), 0)) then return end
-    usePreviousSnap(true)
-    local all = findAll(Pattern("stone.png"):similar(0.9), 0)
-    usePreviousSnap(false)
-    for i, t in ipairs(all) do
-        t:highlight()
-    end
-    wait(1)
-end
-
 -- ========== Settings ================
 Settings:setCompareDimension(true, 1280)
 Settings:setScriptDimension(true, 1280)
 setImmersiveMode(true)
+localPath = scriptPath()
+dofile(localPath.."lib/COCLib.lua")
 Settings:set("MinSimilarity", 0.7)
 setHighlightStyle(0xffffff00, true)
 -- ==========  main program ===========
@@ -44,15 +15,11 @@ collectItems = {"goldCollect.png", "goldCollect2.png",
     "elixirCollect.png", "elixirCollect2.png", "elixirCollect3.png",
     "darkElixirCollect.png"}
 
+collectCount = 1
+reloadCount = 0
+
 while (true) do
---    zoom(200, 600, 600, 400, 1000, 170, 700, 300, 50)
-    zoom(100, 285, 540, 285, 1180, 285, 740, 285, 50)
-    wait(2)
---    zoom(200, 600, 600, 400, 1000, 170, 700, 300, 50)
-    zoom(100, 285, 540, 285, 1180, 285, 740, 285, 50)
-
---eraseStone()
-
+    zoomout()
     while (true) do
 
         -- click on all gold and elixir
@@ -60,28 +27,41 @@ while (true) do
         for i, r in ipairs(collectItems) do
             existsClickAll(r)
         end
+        click(alive)
 
-        if (center:exists(Pattern("disconnect.png"):similar(0.8), 0)) then
+        if (center:exists(Pattern("supercellLogo.png"):similar(0.8), 0)) then
+            getLastMatch():highlight(2)
             toast("Disconnected")
---            getLastMatch():highlight(5)
---            print(getLastMatch():getScore())
-            do return end
             break
         end
+
+        toast("Collect count: " .. collectCount .. "\nReload count: " .. reloadCount)
+        collectCount = collectCount + 1
         wait(1)
-        click(alive)
 
     end
 
     while (true) do
         click(reload)
-        wait(3)
-        if (not center:exists(Pattern("disconnect.png"):similar(0.8), 5)) then break end
+        wait(1)
+        click(alive)
+        wait(1)
+--        if (not center:exists(Pattern("supercellLogo.png"):similar(0.8), 5)) then break end
+        if (exists("trophy.png", 5)) then break end
         toast("Disconnected")
 
-        wait(60)
+        wait(5) --60)
     end
-    wait(5)
---    zoom(200, 600, 600, 400, 1000, 170, 700, 300, 50)
+
+    click(alive)
+    while (existsClick("cancel.png", 0)) do
+        wait(1)
+    end
+    wait(1)
+    click(alive)
+
+
+    reloadCount = reloadCount + 1
+--    exists("trophy.png", 15)
 
 end

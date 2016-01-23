d338af56c08e1fd923df1dae7adf8725d7471c16
AnkuLua <ankulua@gmail.com>
2016-01-23 09:51:08 +0800

    improve zoomout add loadStrings and tableLookup

--- lib/COClib.lua
+++ lib/COClib.lua
@@ -3,19 +3,33 @@ battleRegion = Region(100, 70, 1080, 680)
 
 
 function zoomout()
-    local attack  = find("attack,png")
-    local shop = find("shop.png")
-    swipe(Location(640, 700), Location(640,150))
+    local attack  = find("attack.png"):getTarget()
+    local shop = find("shop.png"):getTarget()
+    local swipeFrom = Location(attack:getX(), attack:getY()-70)
+    local swipeTo = Location(attack:getX(), attack:getY() - 500)
+    swipe(swipeFrom, swipeTo)
     wait(1)
-    swipe(Location(640, 700), Location(640,150))
+    swipe(swipeFrom, swipeTo)
     wait(1)
-    swipe2(Location(640, 700), Location(640,150))
+    swipe(swipeFrom, swipeTo)
     wait(1)
 
-    zoom(150, 700, 540, 700, 1130, 700, 740, 700, 150)
-    wait(2)
-    zoom(150, 700, 540, 700, 1130, 700, 740, 700, 150)
+    local zoom1From = Location(attack:getX() + 70, attack:getY())
+    local zoom1To = Location(540, zoom1From:getY())
+    local zoom2From = Location(shop:getX() - 70, shop:getY())
+    local zoom2To = Location(740, zoom1From:getY())
+--    zoom(150, 700, 540, 700, 1130, 700, 740, 700, 150)
+    zoom(zoom1From:getX(), zoom1From:getY(), zoom1To:getX(), zoom1To:getY(),
+        zoom2From:getX(), zoom2From:getY(), zoom2To:getX(), zoom2To:getY(), 250)
+    wait(1)
+    zoom(zoom1From:getX(), zoom1From:getY(), zoom1To:getX(), zoom1To:getY(),
+        zoom2From:getX(), zoom2From:getY(), zoom2To:getX(), zoom2To:getY(), 250)
+    wait(1)
+    zoom(zoom1From:getX(), zoom1From:getY(), zoom1To:getX(), zoom1To:getY(),
+        zoom2From:getX(), zoom2From:getY(), zoom2To:getX(), zoom2To:getY(), 250)
 
+    wait(1)
+    dragDrop(swipeTo, Location(attack:getX(), 300))
     if (exists("centerLeftCorner.png")) then
         dragDrop(getLastMatch(), find("chat.png"))
         do return end
@@ -77,4 +91,29 @@ function simpleDialog(title, message)
     dialogInit()
     addTextView(message)
     dialogShow(title)
+end
+
+-- ============= strings related ================
+function fileExists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
+function loadStrings(path)
+    local language = getLanguage()
+    local file = path.."strings."..getLanguage()..".lua";
+    if (fileExists(file)) then
+        dofile(file)
+    else
+        if (fileExists(path.."strings.lua")) then
+            dofile(path.."strings.lua")
+        end
+    end
+end
+
+function tableLookup(table, item)
+    for i, t in ipairs(table) do
+        if (t == item) then return i end
+    end
+    return -1
 end
